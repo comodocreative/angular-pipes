e8194c56762f51e8f544e70cedf7fdaca810f60d
Wim Van Cauwenbergh <wim@comodocreative.com>
2016-09-16 20:50:17 +0200

    Updated filter-by pipe

--- filter-by.ts
+++ filter-by.ts
@@ -5,17 +5,19 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class FilterByPipe implements PipeTransform {
 
-	private _filterBy( arr, criteria ) {
-
-		return arr.filter( function ( obj ) {
-			return Object.keys( criteria ).every( function ( c ) {
-				return new RegExp( criteria[c] ).test( obj[c] );
-			});
-		});
-	}
-    
     public transform( arr: Array<any>, args? ) {
 
-        return this._filterBy( arr, args );
+        if (arr) {
+            return this._filterBy( arr, args );
+        }
+    }
+
+    private _filterBy( arr, criteria ) {
+
+        return arr.filter( function ( obj ) {
+            return Object.keys( criteria ).every( function ( c ) {
+                return new RegExp( criteria[c] ).test( obj[c] );
+            });
+        });
     }
 }
