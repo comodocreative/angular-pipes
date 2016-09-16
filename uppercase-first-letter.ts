834efe694b99af0819970fb30b96481cfdb0791a
Wim Van Cauwenbergh <wim@comodocreative.com>
2016-09-16 20:50:56 +0200

    Updated uppercase-first-letter pipe

--- uppercase-first-letter.ts
+++ uppercase-first-letter.ts
@@ -7,7 +7,7 @@ export class UpperCaseFirstLetterPipe implements PipeTransform {
     
     public transform( str: string ) {
 
-        if ( str.length ) {
+        if ( str && str.length ) {
             
             return str.charAt(0).toUpperCase() + str.slice(1);
         }
